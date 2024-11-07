dc9f675ee3fd6c9f35c6e8e671de4d9c7c88a29b
Pedro Luciano de Azevedo <pedro.azevedo@tote.local>
2024-11-07 12:41:37 -0300

    Segunda parte do curso

--- todolist/src/main/java/br/com/pedroluciano/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/pedroluciano/todolist/user/UserController.java
@@ -3,6 +3,9 @@ package br.com.pedroluciano.todolist.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -10,21 +13,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequestMapping("/users")
 
 public class UserController {
-    /**
-     * String (texto)
-     * Integer (int) numeros inteiros
-     * Double (double) Números 0.0000
-     * Float (float) Números 0.000
-     * char (A C)
-     * Date (data)
-     * void
-     */
 
-     /**
-      * Body
-      */
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        
+        if(user != null){
+            System.out.println("Usuario já existe");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
--- todolist/src/main/java/br/com/pedroluciano/todolist/user/UserModel.java
+++ todolist/src/main/java/br/com/pedroluciano/todolist/user/UserModel.java
@@ -1,8 +1,30 @@
 package br.com.pedroluciano.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import lombok.Data;
+
+@Entity(name="tb_users")
+@Data
 public class UserModel {
     
-    public String username;
-    public String name;
-    public String password;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    
+    
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
