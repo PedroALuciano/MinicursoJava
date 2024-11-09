ab1061dcfae9ee06ff04f0818afcb11629bd9441
PedroALuciano <azevedlpedro@gmail.com>
2024-11-09 12:50:33 -0300

    Terceira parte

--- todolist/src/main/java/br/com/pedroluciano/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/pedroluciano/todolist/user/UserController.java
@@ -1,8 +1,13 @@
 package br.com.pedroluciano.todolist.user;
 
+
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,13 +24,16 @@ public class UserController {
 
     @PostMapping("/")
     public ResponseEntity create(@RequestBody UserModel userModel) {
-        var a;
         var user = this.userRepository.findByUsername(userModel.getUsername());
         
         if(user != null){
-            System.out.println("Usuario já existe");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
+        
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
